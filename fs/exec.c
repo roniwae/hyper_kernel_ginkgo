8725c6e801e8b677c32629df4190180ec96e910c
dhelo11 <dhelo11@No-Mercy>
2024-05-14 17:11:17 +0000

    Revert "fs: add kernelsu hook"
    This reverts commit 29408e9fc22f1c27fd2508b43e1e89c5d6921de2.

--- fs/exec.c
+++ fs/exec.c
@@ -1716,12 +1716,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 
-#ifdef CONFIG_KSU
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-#endif
-
 static void android_service_blacklist(const char *name)
 {
 #define FULL(x) { x, sizeof(x) }
@@ -1746,6 +1740,7 @@ static void android_service_blacklist(const char *name)
 	}
 }
 
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1759,9 +1754,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-#ifdef CONFIG_KSU
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
+
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
